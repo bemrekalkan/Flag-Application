cf30724747e1baad4cbb005ec7b43ffa9adbe3e9
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-15 22:02:33 +0300

    .

--- app.js
+++ app.js
@@ -7,24 +7,31 @@ const fetchCountry = async (name) => {
   try {
     const res = await fetch(url);
     if (!res.ok) {
-      renderError(`Something went wrong: ${res.status}`);
+      renderError(`Something went wrong:${res.status}`);
       throw new Error();
     }
     const data = await res.json();
-    console.log(data[0]);
     renderCountry(data[0]);
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const renderError = (err) => {
   const countriesDiv = document.querySelector(".countries");
-  countriesDiv.innerHTML = `<h1 class= "text-danger">${err}</h1> <img src="./404.png" alt=""/>`;
+  countriesDiv.innerHTML = `
+     <h1 class="text-danger">${err}</h1>
+     <img src="./img/404.png" alt="" />
+    `;
 };
 
 const renderCountry = (country) => {
   console.log(country);
+  const countriesDiv = document.querySelector(".countries");
+  const { capital } = country;
+  console.log(capital);
 };
 
 fetchCountry("turkey");
-fetchCountry("usa");
-fetchCountry("france");
+// fetchCountry('usa');
+// fetchCountry('france');
