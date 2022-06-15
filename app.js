2604588b2ca73716e791f72e24390f75deddbaac
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-15 21:53:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//*=========================================================
+//*                     FLAG-APP
+//*=========================================================
+
+const fetchCountry = async (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      renderError(`Something went wrong: ${res.status}`);
+      throw new Error();
+    }
+    const data = await res.json();
+    console.log(data[0]);
+  } catch (error) {}
+};
+
+const renderError = (err) => {
+  const countriesDiv = document.querySelector(".countries");
+  countriesDiv.innerHTML = `<h1 class= "text-danger">${err}</h1> <img src="./404.png" alt=""/>`;
+};
+
+fetchCountry("turkey");
+fetchCountry("usa");
